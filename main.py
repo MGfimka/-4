cfe630f378bc88ef0f4560a401aeab3ecb864078
MGfimka <efimoffmg@yandex.ru>
2024-07-19 16:54:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+my_string = input("Введите Ваше имя: ")
+print(len(my_string))#
+#current_year = 2024
+#date_of_birth = int(input("В каком году Вы родились? "))
+#age = current_year - date_of_birth
+#print("Здравствуйте,", my_string )
+#print("В этом году Вам",age, "лет")
+print(my_string.upper())
+print(my_string.lower())
+my_string_2 = input("Где и как проводите отпуск? ")
+print(my_string_2.replace(" ","#"))
+print(my_string[0])
+print(my_string[-1])
